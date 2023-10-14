85c0290ec84437edd95d2b2580dc69d979fd2404
Valera Demin <wahrwolffe@gmail.com>
2023-10-15 00:52:05 +0300

    add class HeadHunter

---
+++ classes/hh.py
@@ -0,0 +1,40 @@
+import requests
+
+from classes.abstract import GetByApi
+
+
+class HeadHunter(GetByApi):
+    def get_vacancies(self, keyword, num_vacancies=25):
+        __URL = "https://api.hh.ru/vacancies"
+        headers = {'User-Agent': 'api-test-agent'}
+        params = {"text": keyword,
+                  "per_page": num_vacancies,
+                  "area": 1,
+                  "only_with_salary": True,
+                  "currency": "RUR",
+                  }
+        response = requests.get(url=__URL, headers=headers, params=params).json()
+
+        if "items" not in response or not response["items"]:
+            # print(f"К сожалению нет вакансий по запросу: {keyword}")
+            return []
+
+        result = []
+        for item in response["items"]:
+            salary = item.get("salary", None)
+            requirement = item.get("snippet", {}).get("requirement", None)
+            if requirement is not None:
+                requirement = requirement.replace("<highlighttext>", "").replace("</highlighttext>", "")
+            tmp = {
+                "keyword": keyword.upper(),
+                "id": item.get("id", None),
+                "title": item.get("name", None),
+                "url": item.get("alternate_url", None),
+                "salary_from": salary.get("from") if salary and "from" in salary else None,
+                "salary_to": salary.get("to") if salary and "to" in salary else None,
+                "currency": salary.get("currency") if salary and "currency" in salary else None,
+                "requirement": requirement,
+            }
+            result.append(tmp)
+
+        return result
