f7491283ebd8e9440c1bb1a78c5460353be88309
Valera Demin <wahrwolffe@gmail.com>
2023-10-15 01:02:53 +0300

    add class Vacancy

---
+++ classes/vacancy.py
@@ -0,0 +1,67 @@
+class Vacancy:
+
+    vacancies_objs_lst = []
+
+    def __init__(self, data_from_api):
+        self.keyword = data_from_api.get("keyword")
+        self.title = data_from_api.get("title")
+        self.url = data_from_api.get("url")
+        self.salary_from = data_from_api.get("salary_from")
+        self.salary_to = data_from_api.get("salary_to")
+        self.average_salary = self.get_average_salary()
+        self.currency = self.get_format_currency(data_from_api)
+        self.requirement = data_from_api.get("requirement")
+
+        Vacancy.vacancies_objs_lst.append(self)
+
+    def __repr__(self):
+        return (f"{__class__.__name__}(keyword={self.keyword}, title={self.title}, url={self.url},"
+                f"salary_from={self.salary_from}, salary_to={self.salary_to}, average_salary={self.average_salary},"
+                f"currency={self.currency}, requirement={self.requirement})")
+
+    # def __str__(self):
+    #     return (f"Название: {self.title},\nСсылка: {self.url},\nЗарплата: от {self.salary_from} до {self.salary_to}, "
+    #             f"\nОписание: {self.requirement}")
+
+    def get_overview(self):
+        print()
+        print(f"Название - {self.title}")
+        print(f"Ссылка - {self.url}")
+        print(f"Средняя зарплата - {self.average_salary} {self.currency}")
+        print(f"Краткая аннотация - '{self.slice_string(self.requirement, 11)} ...'")
+        print()
+
+    def get_average_salary(self):
+        if self.salary_from is not None and self.salary_to is not None:
+            return round((self.salary_from + self.salary_to) / 2)
+        elif self.salary_from is not None:
+            return self.salary_from
+        elif self.salary_to is not None:
+            return self.salary_to
+        else:
+            return None
+
+    @staticmethod
+    def get_format_currency(data_from_api):
+        if data_from_api["currency"] == "RUR" or data_from_api["currency"] == "rub":
+            currency_upd = "руб."
+            return currency_upd
+        else:
+            return data_from_api.get("currency")
+
+    @staticmethod
+    def slice_string(string, num_spaces):
+        if not string:
+            return ""
+
+        count = 0
+        result = ""
+        for char in string:
+            if char == " ":
+                count += 1
+            if count <= num_spaces:
+                result += char
+            else:
+                break
+        return result
+
