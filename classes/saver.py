59798019440e3453f9d4c2dc79eac6b814a6b67e
Valera Demin <wahrwolffe@gmail.com>
2023-10-15 01:13:41 +0300

    class JsonOut add method

--- classes/saver.py
+++ classes/saver.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from classes.abstract import Saver
 from classes.vacancy import Vacancy
 
@@ -12,3 +15,8 @@ class JsonOut(Saver):
         if isinstance(obj, Vacancy):
             return obj.__dict__
         raise TypeError("Объект типа 'Vacancy' не может быть сериализован в JSON")
+
+    def save_to_file(self, vacancies, filename):
+        file_path = os.path.join(self.data_folder, filename)
+        with open(file_path, 'w') as file:
+            json.dump(vacancies, file, indent=4, default=self.vacancy_serializer, ensure_ascii=False)
