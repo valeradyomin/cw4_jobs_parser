cdcc1f639d2604bd4ca07f16757e5349d8b52383
Valera Demin <wahrwolffe@gmail.com>
2023-10-15 00:49:28 +0300

    add class Saver

--- classes/abstract.py
+++ classes/abstract.py
@@ -5,3 +5,9 @@ class GetByApi(ABC):
     @abstractmethod
     def get_vacancies(self, keyword, num_vacancies):
         pass
+
+
+class Saver(ABC):
+    @abstractmethod
+    def save_to_file(self, vacancies, filename):
+        pass
