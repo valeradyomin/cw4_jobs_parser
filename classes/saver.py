c0a0052546fe4ca64d8c703a7a40b0adbbb32541
Valera Demin <wahrwolffe@gmail.com>
2023-10-15 01:12:39 +0300

    class JsonOut add staticmethod

--- classes/saver.py
+++ classes/saver.py
@@ -1,7 +1,14 @@
 from classes.abstract import Saver
+from classes.vacancy import Vacancy
 
 
 class JsonOut(Saver):
     def __init__(self, vacancies):
         self.vacancies = vacancies
         self.data_folder = "./_resulting_data"
+
+    @staticmethod
+    def vacancy_serializer(obj):
+        if isinstance(obj, Vacancy):
+            return obj.__dict__
+        raise TypeError("Объект типа 'Vacancy' не может быть сериализован в JSON")
